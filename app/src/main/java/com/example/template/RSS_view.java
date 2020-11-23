4398935b2e9f277a51d2bce9631fccc32354966a
JEdwardsD <jedwardsd@protonmail.com>
2020-11-22 22:51:38 -0500

    Ignore this commit

---
+++ app/src/main/java/com/example/template/RSS_view.java
@@ -0,0 +1,14 @@
+package com.example.template;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RSS_view extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_rss_view);
+    }
+}
