274dd9a2ae245714be47fe67a9a66d0929192298
Noah Jerrell Jackson <56763492+Noahjackson512@users.noreply.github.com>
2020-12-03 15:19:32 -0500

    Add files via upload

--- app/src/main/java/com/example/template/MainActivity.java
+++ app/src/main/java/com/example/template/MainActivity.java
@@ -1,23 +1,53 @@
-package com.example.template;
+package com.example.pokemonconcept;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner pokemon1;
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private ArrayList<String> arrayList= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pokemon1 = findViewById(R.id.pokemon1);
+        showDataSpinner();
+
     }
 
-    public void gotoIndView(View view) {
-        Intent intent = new Intent(this, Individual_Pokemon_view.class);
-        startActivity(intent);
-    }
+    private void showDataSpinner() {
+        databaseReference.child("Pokemon").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                arrayList.clear();
+                for(DataSnapshot item : dataSnapshot.getChildren()){
+                    arrayList.add(item.child("Name").getValue(String.class));
+                }
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this,R.layout.style_spinner,arrayList);
+                pokemon1.setAdapter(arrayAdapter);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 
 }
