5eec52830848a00cda17d2b5c3c09c6d4a5fa1bf
jacobletizia <jletizia@email.sc.edu>
2021-04-04 23:14:50 -0400

    comments on PokedexView and PersonalDex, changed color of spinner background and text box backgrounds for add pokemon

--- app/src/main/java/com/example/template/PersonalDex.java
+++ app/src/main/java/com/example/template/PersonalDex.java
@@ -2,30 +2,17 @@ package com.example.template;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -35,8 +22,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.jar.Attributes;
 
+// this class was coded in its entirety by Jacob Letizia in March 2021
 public class PersonalDex extends AppCompatActivity {
     private final FirebaseDatabase fb = FirebaseDatabase.getInstance();
     DatabaseReference db = fb.getReference();
@@ -48,21 +35,19 @@ public class PersonalDex extends AppCompatActivity {
 
     // we will use an ArrayList in our listview because users can add and subtract pokemon, so the size must be changable
     ArrayList<String> arrList = new ArrayList<>();
+
     // array adapters are used to link Arrays and listviews
     ArrayAdapter<String> arrayAdapter;
 
 
-    // this is the code for transitioning between views with the buttons on the bottom
-    public void gotoAddView(View view) {
-        Intent intent = new Intent(this, team_builder.class);
-        startActivity(intent);
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_dex);
 
+        // if the user isn't logged in, we will display this toast
         Context context = getApplicationContext();
         CharSequence dispText = "You are not currently logged in. Log in to add Pokemon to your Pokedex.";
         int duration = Toast.LENGTH_LONG;
@@ -72,8 +57,10 @@ public class PersonalDex extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrList);
         listView.setAdapter(arrayAdapter);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
         // if user is null, no one is logged in, if user isn't null, then someone is logged in
         if (user != null) {
+            // this method accesses the database
             db.addValueEventListener(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -82,20 +69,20 @@ public class PersonalDex extends AppCompatActivity {
                     String currMonName = "";
                     String userName = FirebaseAuth.getInstance().getCurrentUser().getEmail();
                     userName = userName.replace('.', ',');
-                    //Log.e("username", userName);
 
-                    //Log.e("name", "user name: " + userName);
+
                     // this should return the number of pokemon the user currently have
                     // I am subtracting 2 because we populate each user with 2 dummy pokemon
                     long numInDex = snapshot.child("users").child(userName).child("pokedex").getChildrenCount();
-                    //Log.e("dex number", "number of pokemon counted: " + numInDex);
+                    // this temp value will be for display purposes, it will go before each pokemon
                     int temp = 1;
+                    // for each loop so that we can get to access each pokemon
                     for (DataSnapshot ds: snapshot.child("users").child(userName).child("pokedex").getChildren()) {
                         currMonName = ds.getKey();
                         if (!currMonName.equals("pkm1"))
                             if (!currMonName.equals("pkm2")) {
-                                // we will be making 2 array lists, one of the names for displaying and ...
-                                // one of the stats of each pokemon so that we can pass that information along to the individual screen
+                                /* we will be making 2 array lists, one of the names for displaying and
+                                one of the stats of each pokemon so that we can pass that information along to the individual screen*/
                                 Stats tempStats = new Stats();
                                 Long tempAtk = (long)ds.child("stats").child("atk").getValue();
                                 tempStats.setAtk(tempAtk.intValue());
@@ -116,15 +103,8 @@ public class PersonalDex extends AppCompatActivity {
                                 pokemonStats.add(tempStats);
                                 arrList.add("" + temp + ".\t" + currMonName);
                                 temp++;
-
                             }
                     }
-                    /*
-                    for (int i = 0; i < arrList.size(); i++) {
-                        String temp = arrList.get(i);
-                        Log.e("error", temp);
-                    }*/
-
                     listView = (ListView)findViewById(R.id.listViewPersonalDex);
                     arrayAdapter.notifyDataSetChanged();
                 }
@@ -134,14 +114,15 @@ public class PersonalDex extends AppCompatActivity {
                     Log.e("error","error reading");
                 }
             });
-
+            // when a pokemon is clicked, we will be passing in a lot of information
             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    // 1. Mewtwo
-                    // 0123
                     String temp = arrList.get(position).substring(3);
+                    // we will be going to the Individual Pokemon view
                     Intent intent = new Intent(getBaseContext(), Individual_Pokemon_view.class);
+                    /* we will be passing all the current stats for the pokemon to the next screen so that
+                    they don't need to be loaded again */
                     intent.putExtra("pkmnHP",pokemonStats.get(position).getHp() + "");
                     intent.putExtra("pkmnSPD",pokemonStats.get(position).getSpd() + "");
                     intent.putExtra("pkmnATK",pokemonStats.get(position).getAtk() + "");
@@ -149,16 +130,23 @@ public class PersonalDex extends AppCompatActivity {
                     intent.putExtra("pkmnSATK",pokemonStats.get(position).getSatk() + "");
                     intent.putExtra("pkmnSDEF",pokemonStats.get(position).getSdef() + "");
 
+                    // nickname is the name of the pokemon that will be going into the next screen
                     intent.putExtra("nickname", temp);
                     String tempID = pokemonIDs.get(position);
                     intent.putExtra("pictureID", "icon" + tempID);
                     startActivity(intent);
-                    Toast.makeText(PersonalDex.this, arrList.get(position) + "", Toast.LENGTH_LONG).show();
+                    //Toast.makeText(PersonalDex.this, arrList.get(position) + "", Toast.LENGTH_LONG).show();
                 }
             });
         }
         else // user isn't logged in
             notLoggedInToast.show();
+    }
+
+    // this is the code for transitioning between views with the buttons on the bottom
+    public void gotoAddView(View view) {
+        Intent intent = new Intent(this, team_builder.class);
+        startActivity(intent);
     }
 
     public void gotoDexView(View view) {
--- app/src/main/java/com/example/template/team_builder.java
+++ app/src/main/java/com/example/template/team_builder.java
@@ -22,6 +22,7 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -123,12 +124,12 @@ public class team_builder extends AppCompatActivity {
         }
 
         // setting up the spinner for the natures
-        natureAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_spinner_item, natures);
+        natureAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, natures);
         natureAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         natureSpinner.setAdapter(natureAdapter);
 
         // setting up the spinners for the abilities
-        abilityAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_spinner_item, abilities);
+        abilityAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, abilities);
         abilityAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         abilitySpinner.setAdapter(abilityAdapter);
 
